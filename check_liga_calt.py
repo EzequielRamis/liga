13f9adf398e75cfc3d0cff8d16df562d66a4665b
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-12-02 18:22:20 -0300

    curated features

---
+++ check_liga_calt.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from glyphsLib import GSFont
+import re
+from features_sample import features
+
+
+def check():
+    font = GSFont("fira.glyphs")
+    liga_feature = next((i for i in font.features if i.name == "liga"), "")
+    ligas = set(re.findall("(?<=by )([\s\S]*?)(?=.liga)", liga_feature.code))
+    calts = set(features["calt"])
+    if ligas.issubset(calts):
+        print("OK")
+    else:
+        print("ERROR")
+        print(ligas.symmetric_difference(calts))
+
+
+def main():
+    check()
+
+
+if __name__ == "__main__":
+    main()
